df50e7b50c9e58b9301aaf979f052afafcb5c896
PedroDominguezUTEC <pedro.dominguez@utec.edu.pe>
2022-03-31 21:16:37 -0500

    Tax.java - Refactoring conditionals - Function throws exception if it receives a non-String * A new vector holding alpha country codes can be added (overkill?)

--- src/main/java/lab/demand/Tax.java
+++ src/main/java/lab/demand/Tax.java
@@ -1,15 +1,15 @@
 package lab.demand;
 
 public class Tax {
-    
-    public double calculateTax(String country) {
-        if (country.equals("PE")) {
-            return 0.18;
-        } else if (country.equals("BR")) {
-            return 0.12;
-        } else {
-            return 0.0;
-        }
-    }
 
+    public double calculateTax(String country) throws IllegalArgumentException {
+        if (!(country instanceof String)) {
+            throw new IllegalArgumentException("calculateTax() only receives Alpha-2 country codes");
+        }
+        if (country.equals("PE"))
+            return 0.18;
+        if (country.equals("BR"))
+            return 0.12;
+        return 0.0;
+    }
 }
